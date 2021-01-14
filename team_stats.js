0f284652ad4dc7e011cf8d4c214464c7ffa10031
Joshua Rios <1984yearoftherat@gmail.com>
2021-01-13 15:33:16 -1000

    finished refactoring

--- team_stats.js
+++ team_stats.js
@@ -1,4 +1,25 @@
-const team = {};
+const team = {
+    addPlayer(firstName, lastName, age) {
+        this._players.push({
+            firstName: firstName,
+            lastName: lastName,
+            age: age
+        })
+    },
+    addGame(opponentName, yourTeamsPts, opponentPts) {
+        this._games.push({
+            opponent: opponentName,
+            teamPoints: yourTeamsPts,
+            opponentPoints: opponentPts
+        })
+    },
+    get games() {
+        return this._games;
+    },
+    get players() {
+        return this._players;
+    }
+};
 team._players = [];
 team._games= [];
 team._players.push({
@@ -31,30 +52,7 @@ team._games.push({
     teamPoints: 35,
     opponentPoints: 21
 });
-Object.defineProperties(team, {
-    games: {get: function() {
-        return this._games;
-    }}
-});
-Object.defineProperties(team, {
-    players: {get: function() {
-        return this._players;
-    }}
-});
-team.addPlayer = function(firstName, lastName, age) {
-    this._players.push({
-        firstName: firstName,
-        lastName: lastName,
-        age: age
-    })
-};
-team.addGame = function(opponentName, yourTeamsPts, opponentPts) {
-    this._games.push({
-        opponent: opponentName,
-        teamPoints: yourTeamsPts,
-        opponentPoints: opponentPts
-    })
-};
+
 team.addPlayer('Joshua', 'Rios', 36);
 team.addPlayer('Steph', 'Curry', 28);
 team.addPlayer('Lisa', 'Leslie', 44);
@@ -62,4 +60,4 @@ team.addPlayer('Bugs', 'Bunny', 76);
 team.addGame('Redskins', 66, 20);
 team.addGame('Beavers', 6, 21);
 team.addGame('Bears', 31, 4);
-console.log(team.games);
+console.log(team.players);
